4225188c7b8855c24d60cc39d8c40988dd48f54c
coup321 <coup321@gmail.com>
2024-04-14 19:49:52 -0400

    renamed files

---
+++ name_aura/NameTrigger.lua
@@ -0,0 +1,15 @@
+function(allstates, ...)
+    local event, name = ...
+    if event == "DEATHLOG_WA" then
+        allstates["PLAYERNAME"] = {
+            show = true,
+            changed = true,
+            autoHide = true,
+            duration = 5,
+            expirationTime = GetTime() + 5,
+            playerName = name
+        }
+        return true
+    end
+    
+end
