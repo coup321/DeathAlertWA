79b3df7b21f5870d5a71eba9f386524b7aa35af6
coup321 <coup321@gmail.com>
2024-04-16 19:53:25 -0400

    added max number of rows per death

--- Group/GroupGrow.lua
+++ Group/GroupGrow.lua
@@ -1,26 +1,43 @@
 function(newPositions, activeRegions)
+    -- get config from Bars aura
     local config
     for i, env in pairs(aura_env.child_envs) do
         if env.id == "Bars" then
             config = env.config
         end
     end
-    local mdiP1Width = 0
 
+    -- get width of the first half of the MDI style string
+    -- can't guarantee that part 1 will come first, so need to iterate through
+    local mdiP1Width = 0
     for _, regionData in pairs(activeRegions) do
         local id = regionData.id
         if id == "MDI" then
             mdiP1Width = regionData.dimensions.width
             break
         end
-
     end
+
+    -- calculate rows per player death
+    local isNameShown = config.displaySimplePlayerName and 1 or 0
+    local isMdiStringShown = config.displayDeathText and 1 or 0
+    local isBarsShown = config.displayBars and 1 or 0
+    local barsPerDeath = 4
+    local rowsPerDeath = barsPerDeath*isBarsShown + 2*isMdiStringShown + isNameShown
+    -- max number of rows must be calculated based on 
+        -- if name is shown (1 row)
+        -- if MDI string is shown (2 rows)
+        -- how many bars are allowed (n rows) - 4 is default
+    local maxNumberOfRows = rowsPerDeath * math.floor(config.maxNumberOfRows / rowsPerDeath)
+
+    -- define an MDI index, since it'll need to be used twice 
+    -- one time for each MDI string part (1 and 2)
     local mdiIndex = nil
     for i, regionData in ipairs(activeRegions) do
-        if i > config.maxNumberOfRows then
+
+        if i > maxNumberOfRows then -- there are more active auras than allowed rows
             break
         end
-
 
         local width = regionData.dimensions.width / 2
         local id = regionData.id
