7ed83cf7bae79f248f181e924eeed91a244261ce
coup321 <coup321@gmail.com>
2024-04-17 19:19:50 -0400

    this should fix the error of indexing ? damage type from testing

--- bars/BarOnLoad.lua
+++ bars/BarOnLoad.lua
@@ -80,7 +80,7 @@ function DamageEvent:damageColorString()
         [32] = { name = "Shadow", color = "8080FF" },
         [64] = { name = "Arcane", color = "FF80FF" },
     }
-    local typeColor =  damageTypes[self.damageType]['color']
+    local typeColor = damageTypes and damageTypes[self.damageType]['color'] or nil
     return typeColor and typeColor or "FFFf00"
 end
 
