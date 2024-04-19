6a0e291ee2889c80276298a08efc57f93eb89c2a
coup321 <coup321@gmail.com>
2024-04-19 16:06:21 -0400

    added historySize option

--- Group/GroupGrow.lua
+++ Group/GroupGrow.lua
@@ -1,9 +1,12 @@
 function(newPositions, activeRegions)
     -- get config from Bars aura
     local config
+    local height
     for i, env in pairs(aura_env.child_envs) do
         if env.id == "Bars" then
+            print("apparently there are some bars here????")
             config = env.config
+            height = env.region.height
         end
     end
 
@@ -11,17 +14,20 @@ function(newPositions, activeRegions)
     local isNameShown = config.displaySimplePlayerName and 1 or 0
     local isMdiStringShown = config.displayDeathText and 1 or 0
     local isBarsShown = config.displayBars and 1 or 0
-    local barsPerDeath = 4
+    local barsPerDeath = config.historySize
     local rowsPerDeath = barsPerDeath*isBarsShown + 2*isMdiStringShown + isNameShown
     -- max number of rows must be calculated based on 
         -- if name is shown (1 row)
         -- if MDI string is shown (2 rows)
         -- how many bars are allowed (n rows) - 4 is default
     local maxNumberOfRows = rowsPerDeath * config.numberOfDeathsToShow
+    local barPadding = config.barPadding
+    height = height + barPadding
 
     local mdiIndex = nil
     local mdiP1Width = 0
-    for i = 1, #activeRegions do 
+    local offSet = 0
+    for i = 1, #activeRegions do
         local regionData = activeRegions[i]
         local j = #activeRegions > maxNumberOfRows and i - (#activeRegions - maxNumberOfRows) or i
         local width = regionData.dimensions.width / 2
@@ -36,12 +42,12 @@ function(newPositions, activeRegions)
         end
 
         local show = i > #activeRegions - maxNumberOfRows
-
+            
         if id == "MDI" then
             mdiIndex = j + 1
-            newPositions[i] = {width, -1*mdiIndex*25, show}
+            newPositions[i] = {width, -1*mdiIndex*height, show}
         else
-            newPositions[i] = {width, -1*j*25, show}
+            newPositions[i] = {width, -1*j*height, show}
         end
     end
 end
--- bars/BarTrigger.lua
+++ bars/BarTrigger.lua
@@ -1,6 +1,6 @@
 function (allstates, ...)
     local eventHandler = aura_env.eventHandler
-    local historySize = 4
+    local historySize = aura_env.config.historySize
     local activePlayerDied = eventHandler:process(historySize, ...)
     if activePlayerDied == true then
         local newStates = eventHandler:death(...)
