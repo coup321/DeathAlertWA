cde9116acf43ab9810dea78f2e20fd3caa4f5db1
coup321 <coup321@gmail.com>
2024-04-19 21:49:04 -0400

    fixed sorting when only showing MDI strings

--- Group/GroupGrow.lua
+++ Group/GroupGrow.lua
@@ -4,7 +4,6 @@ function(newPositions, activeRegions)
     local height
     for i, env in pairs(aura_env.child_envs) do
         if env.id == "Bars" then
-            print("apparently there are some bars here????")
             config = env.config
             height = env.region.height
         end
@@ -26,10 +25,12 @@ function(newPositions, activeRegions)
 
     local mdiIndex = nil
     local mdiP1Width = 0
-    local offSet = 0
+    local offset = -1
+    local isReset = false
     for i = 1, #activeRegions do
         local regionData = activeRegions[i]
         local j = #activeRegions > maxNumberOfRows and i - (#activeRegions - maxNumberOfRows) or i
+        j = j + math.floor((j-1)/rowsPerDeath)
         local width = regionData.dimensions.width / 2
         local id = regionData.id
 
@@ -42,11 +43,21 @@ function(newPositions, activeRegions)
         end
 
         local show = i > #activeRegions - maxNumberOfRows
-            
-        if id == "MDI" then
-            mdiIndex = j + 1
+
+        -- reset the offset when you actually start showing rows
+        if i > #activeRegions - maxNumberOfRows and not isReset then
+            offset = -1
+            isReset = true
+        end
+        -- if it's the second MDI item, then set the index to be j - 1
+        -- if it's not the first MDI item, then it also needs offset added to it
+        -- iterate offset down one
+        if id == "MDI2" then
+            mdiIndex = j - 1 + offset
+            offset = offset - 1
             newPositions[i] = {width, -1*mdiIndex*height, show}
         else
+            j = j + offset
             newPositions[i] = {width, -1*j*height, show}
         end
     end
