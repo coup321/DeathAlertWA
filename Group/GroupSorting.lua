3bda9affeab9b4953593f8deabaf2082f601090e
coup321 <coup321@gmail.com>
2024-04-21 14:14:41 -0400

    change sorting function to guarentee MDI is before MDI2

--- Group/GroupSorting.lua
+++ Group/GroupSorting.lua
@@ -1,6 +1,13 @@
 function(a, b)
+    -- guarentees that MDI comes before MDI2 for the grow function
+    if a.region.id == "MDI2" and b.region.id == "MDI" then
+        return true
+    end
+
     local a = a.region.state.sortIndex
     local b = b.region.state.sortIndex
+
+
     if  a and b then
         return a <= b
     end
