8799ea800aacd4823112d626091164d5a7c61341
coup321 <coup321@gmail.com>
2024-04-21 18:09:22 -0400

    renamed file

---
+++ mdi/MdI2Text.lua
@@ -0,0 +1,14 @@
+function()
+    -- unitId, abilityName, amount, sourceName
+    if aura_env.state.sourceName then 
+        local abilityName = aura_env.state.abilityName
+        local amount = aura_env.state.amount
+        local sourceName = aura_env.state.sourceName
+        local overkill = aura_env.state.overkill
+        local damageColorString = aura_env.state.damageColorString
+        local coloredAbilityName = "|cFF" .. damageColorString .. abilityName .. "|r"
+        local coloredAmount = "|cFF".. damageColorString .. amount .. "|r"
+        return coloredAbilityName .. " from " .. sourceName .. " for " .. coloredAmount .. overkill
+
+    end 
+end
